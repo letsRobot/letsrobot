b461e74ef9edeb7eb2f82fb8dcc43fda061c0d70
Peter Steele <psteele@veraida.com>
2018-10-15 14:22:22 +1100

    Updated the sample conf with a missing key, and hardware/serial_board.py to the latest version.

--- hardware/serial_board.py
+++ hardware/serial_board.py
@@ -3,18 +3,13 @@ import serial
 import sys
 
 ser = None
+debug_messages = None
 
 def sendSerialCommand(ser, command):
 
-
-    print(ser.name)         # check which port was really used
-    ser.nonblocking()
-
-    # loop to collect input
-    #s = "f"
-    #print("string:", s)
-    print(str(command.lower()))
-    ser.write(command.lower().encode('utf8') + "\r\n")     # write a string
+    if debug_messages:
+       print("serial send: ", str(command.lower()))
+    ser.write(command.lower().encode('utf8') + b"\r\n")     # write a string
     #ser.write(s)
     ser.flush()
 
@@ -25,43 +20,45 @@ def sendSerialCommand(ser, command):
 
 def setup(robot_config):
     global ser
-    serialDevice = robot_config.get('serial', 'serial_device')
-    # initialize serial connection
+    global debug_messages
 
-#TODO add baud rate to config file
-    serialBaud = robot_config.get('serial', 'serial_baud')
-    print("baud:", serialBaud)
+    serial_device = robot_config.get('serial', 'serial_device')
+    debug_messages = robot_config.getboolean('misc', 'debug_messages')
+    serialBaud = robot_config.getint('serial', 'baud_rate')
+
+    # initialize serial connection
     try:
-        ser = serial.Serial(serialDevice, serialBaud, timeout=1)  # open serial
+        ser = serial.Serial(serialDevice, serialBaud, timeout=0, write_timeout=0)  # open serial
     except:
         print("error: could not open serial port")
         try:
-            ser = serial.Serial('/dev/ttyACM0', serialBaud, timeout=1)  # open serial
+            ser = serial.Serial('/dev/ttyACM0', serialBaud, timeout=0, write_timeout=0)  # open serial
         except:
             print("error: could not open serial port /dev/ttyACM0")
             try:
-                ser = serial.Serial('/dev/ttyUSB0', serialBaud, timeout=1)  # open serial
+                ser = serial.Serial('/dev/ttyUSB0', serialBaud, timeout=0, write_timeout=0)  # open serial
             except:
                 print("error: could not open serial port /dev/ttyUSB0")
                 try:
-                    ser = serial.Serial('/dev/ttyUSB1', serialBaud, timeout=1)  # open serial
+                    ser = serial.Serial('/dev/ttyUSB1', serialBaud, timeout=0, write_timeout=0)  # open serial
                 except:
                     print("error: could not open serial port /dev/ttyUSB1")
                     try:
-                        ser = serial.Serial('/dev/ttyUSB2', serialBaud, timeout=1)  # open serial
+                        ser = serial.Serial('/dev/ttyUSB2', serialBaud, timeout=0, write_timeout=0)  # open serial
                     except:
                         print("error: could not open serial port /dev/ttyUSB2")
 
     if ser is None:
         print("error: could not find any valid serial port")
         sys.exit()
-        
+   
+    if debug_messages: 
+       print("Serial Connected")
+       print("port:", ser.name)
+       print("baud:", serialBaud)
+
     return(ser)
     
 def move(args):
     command = args['command']
-    # need to verify args for key position
-    #if robot_config.get('serial', 'serial_keyposition') && args['keyposition']:
-        #command = args['command']+'_'+args['keyposition']
     sendSerialCommand(ser, command)
-	
