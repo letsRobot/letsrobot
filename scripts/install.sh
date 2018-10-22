620f85194650094a9acda5e4efe1c28868d19a50
Bryan Morrison <skeeter_mcbee@live.com>
2018-10-21 19:08:11 -0700

    update install

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,46 +8,46 @@ echo
 
 echo -e "\e[31m**************************************************"
 echo -e "\e[31m* \e[39mYou are now about to install everything needed \e[31m*"
-echo -e "\e[31m* \e[39mto get your robot connected to runmyrobot.com  \e[31m*"
+echo -e "\e[31m* \e[39mto get your robot connected to letsrobot.tv    \e[31m*"
 echo -e "\e[31m* \e[39mBefore we can start, you need to get a robot,  \e[31m*"
 echo -e "\e[31m* \e[39mand camera ID. You can get that by pressing    \e[31m*"
-echo -e "\e[31m* \e[39mthe \"connect your robot\" button                \e[31m*"
+echo -e "\e[31m* \e[39mthe \"connect your robot\" button.             \e[31m*"
 echo -e "\e[31m**************************************************"
 
 echo
 
-echo -e "\e[33mPlease enter your Robot ID:\e[39m "
-read input_robot
+#echo -e "\e[33mPlease enter your Robot ID:\e[39m "
+#read input_robot
 
-re='^[0-9]+$'
-if ! [[ $input_robot =~ $re ]] ; then
-   echo "Error: Robot ID is not a number" >&2; exit 1
-fi
-
-echo
-
-echo -e "\e[33mPlease enter your Camera ID:\e[39m "
-read input_camera
+#re='^[0-9]+$'
+#if ! [[ $input_robot =~ $re ]] ; then
+#   echo "Error: Robot ID is not a number" >&2; exit 1
+#fi
 
 echo
-echo
 
-if ! [[ $input_camera =~ $re ]] ; then
-   echo "Error: Camera ID is not a number" >&2; exit 1
-fi
+#echo -e "\e[33mPlease enter your Camera ID:\e[39m "
+#read input_camera
+#
+#echo
+#echo
+#
+#if ! [[ $input_camera =~ $re ]] ; then
+#   echo "Error: Camera ID is not a number" >&2; exit 1
+#fi
 
-echo -e "\e[33mThank you, sit back and relax, we'll see you on runmyrobot.com\e[39m"
-echo
-sleep 3s
+#echo -e "\e[33mThank you, sit back and relax, we'll see you on letsrobot.tv\e[39m"
 
 # Write the start_robot file with the ID for robot and camera in
-echo '#!/bin/bash' >> ~/start_robot
-echo '# suggested use for this:' >> ~/start_robot
-echo '# (1) Put in the ids for your robot, YOURROBOTID and YOURCAMERAID' >> ~/start_robot
-echo '# (2) use sudo to create a crontab entry: @reboot /bin/bash /home/pi/start_robot' >> ~/start_robot
-echo 'cd /home/pi/letsrobot' >> ~/start_robot
-echo "nohup scripts/repeat_start python reverse_ssh.py ${input_robot} &> /dev/null &" >> ~/start_robot
-echo "nohup scripts/repeat_start python letsrobot.py ${input_robot} &> /dev/null &" >> ~/start_robot
+echo > start_robot <<EOF
+#!/bin/bash
+# suggested use for this:
+# (1) Put in the id's for your robot, YOURROBOTID and YOURCAMERAID
+# (2) use sudo to create a crontab entry: @reboot /bin/bash /home/pi/start_robot
+
+cd /home/pi/letsrobot
+nohup scripts/repeat_start python letsrobot.py &> /dev/null &
+EOF
 
 # Make sure the system is up to date
 sudo apt-get -y update
@@ -56,49 +56,24 @@ sudo apt-get -y update
 #sudo apt-get -y upgrade
 #sudo apt-get -y dist-upgrade
 
-# Start installing everything needed
-sudo apt-get -y install python-serial python-dev libgnutls28-dev espeak python-smbus python-pip git
+# Install required libraries and tools
+sudo apt-get install ffmpeg python-serial python-dev libgnutls28-dev espeak python-smbus python-pip libttspico-utils git
 
-sudo pip install -U socketIO-client &&\
+# Download letsrobot scripts
+git clone https://github.com/letsrobot/letsrobot.git /home/$USER/letsrobot
 
-cd ~ &&\
+# Install python requirements
+sudo python -m pip install -r /home/$USER/letsrobot/requirements.txt
 
-git clone https://github.com/adafruit/Adafruit-Motor-HAT-Python-Library.git &&\
-cd Adafruit-Motor-HAT-Python-Library &&\
+# Copy letsrobot.sample.conf to letsrobot.conf
+cp /home/$USER/letsrobot/letsrobot.sample.conf /home/$USER/letsrobot/letsrobot.conf
 
-sudo apt-get -y install emacs &&\
-sudo apt-get -y install python-dev &&\
-sudo python setup.py install &&\
+echo -e "\e[33mIt is now time to configure the controller. Please go to https://github.com/letsRobot/letsrobot#configure-the-controller for more information.\[e39m"
 
-cd ~ &&\
-wget ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.0.25.tar.bz2 &&\
-tar xjf alsa-lib-1.0.25.tar.bz2 &&\
-cd alsa-lib-1.0.25 &&\
-./configure --host=arm-unknown-linux-gnueabi &&\
-make -j4 &&\
-sudo make install &&\
+nano /home/$USER/letsrobot/letsrobot.conf
 
-cd ~ &&\
-git clone git://git.videolan.org/x264 &&\
-cd x264 &&\
-./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl &&\
-make -j4 &&\
-sudo make install &&\
+cp /home/$USER/letsrobot/scripts/start_robot /home/$USER/start_robot
 
-cd ~ &&\
-git clone https://github.com/FFmpeg/FFmpeg.git &&\
-cd FFmpeg &&\
-./configure --arch=armel --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --extra-libs=-ldl &&\
-make -j4 &&\
-sudo make install
+echo "@reboot /bin/bash /home/pi/start_robot" >> /var/spool/cron/crontabs/pi
 
-cd ~ &&\
-git clone https://github.com/letsrobot/letsrobot &&\
-
-# Add start_robot script to crontab, it might throw an error, but it works anyways
-sudo crontab -l | { cat; echo "@reboot /bin/bash /home/pi/start_robot"; } | sudo crontab -
-
-echo
-echo
-
-echo -e "\e[33mInstall has completed, please run start_robot, or reboot your robot to bring it online.\e[39m"
+echo -e "\e[33mInstall is now complete. Please reboot your robot. See you on letsrobot.tv!\e[39m"
