8753899056ac01dde05d95270d1193394fda4741
Peter Steele <steele.peter@gmail.com>
2018-12-09 02:41:15 +1100

    Updated HUD with a few new elements.

--- video/ffmpeg-hud.py
+++ video/ffmpeg-hud.py
@@ -8,8 +8,14 @@ import logging
 import time
 import os
 from PIL import Image, ImageDraw, ImageFont
+import extended_command
+import psutil
 
 log = logging.getLogger('LR.video.ffmpeg-hud')
+
+hud_correct = False
+hud_correct_x = 0
+hud_correct_y = 0
 
 # This is a pretty ugly way to overload the startAudioCapture(), but it's also
 # the only way that seems to work. Otherwise, when called internally to the 
@@ -20,6 +26,8 @@ def setup(robot_config):
     ffmpeg.setup(robot_config)
     ffmpeg.startVideoCapture=startVideoCapture
 
+    extended_command.add_command('.hud', HUDChatHandler)
+
 def setupHUD(robot_config):
  
     # do additional setup stuff
@@ -28,8 +36,9 @@ def setupHUD(robot_config):
 def startVideoCapture():
     global x_res
     global y_res
-    x_res = ffmpeg.y_res
-    y_res = ffmpeg.x_res
+    global hud_correct
+    x_res = ffmpeg.x_res
+    y_res = ffmpeg.y_res
 
     # set brightness
     if (ffmpeg.brightness is not None):
@@ -72,9 +81,16 @@ def startVideoCapture():
     ffmpeg.video_process=ffmpeg.subprocess.Popen(ffmpeg.shlex.split(videoCommandLine), stdin=ffmpeg.subprocess.PIPE)
     ffmpeg.atexit.register(ffmpeg.atExitVideoCapture)
     #ffmpeg.video_process.wait()
+    HUD = drawBaseHUD()
 
     while True:
-        image = drawHUD()
+        if hud_correct:
+            x_res = hud_correct_x
+            y_res = hud_correct_y
+            hud_correct = False
+            HUD = drawBaseHUD()
+
+        image = drawHUD(HUD)
         try:
             image.save(ffmpeg.video_process.stdin, 'PNG')
         except (IOError, ValueError):
@@ -89,6 +105,25 @@ def startVideoCapture():
     except AttributeError:
         log.debug("exception closing ffmpeg thread and pipe")
         pass
+
+def HUDChatHandler(command, args):
+    global hud_correct
+    global hud_correct_x
+    global hud_correct_y
+
+    if len(command) > 3:
+        if extended_command.is_authed(args['name']): # Moderator
+            if command[1] == 'correct':
+                try:
+                    new_x_res = int(command[2])
+                    new_y_res = int(command[3])
+                except ValueError:
+                    return #Not a number
+                log.info("correcting HUD resolution to %dx%d", new_x_res, new_y_res)
+                hud_correct_x = new_x_res
+                hud_correct_y = new_y_res
+                hud_correct = True
+
 
 def measure_temp():
     temp = os.popen("vcgencmd measure_temp").readline()
@@ -111,12 +146,39 @@ def getWifiStats():
     else:
         return "Not Found"
 
-def drawHUD():
+def drawrect(drawcontext, xy, outline=None, width=0):
+    (x1, y1), (x2, y2) = xy
+    points = (x1, y1), (x2, y1), (x2, y2), (x1, y2), (x1, y1)
+    drawcontext.line(points, fill=outline, width=width)
+
+# This draws all the basic elements / layout of the hud that does not change.
+# It is drawn once. 
+def drawBaseHUD():
     image = Image.new("RGBA", (x_res, y_res), color = (0,0,0,0))
+    image_draw = ImageDraw.Draw(image)
+
+    logo_pos = x_res / 6
+    logo = Image.open('video/LRWebLogox256.png')
+    logo.thumbnail((logo_pos, logo_pos))
+    image.paste(logo, (x_res - logo_pos - 6, 9), logo)
+    drawrect(image_draw, [(2,2),(x_res-4,y_res-4)], outline=(0,255,0), width=3)
+    image_draw.line(((3,y_res-25),(x_res-3, y_res-25)), fill=(0,255,0), width=3)
+    image_draw.line(((logo_pos, y_res-4),(logo_pos, y_res-25)), fill=(0,255,0), width=3)
+    image_draw.line(((logo_pos*5, y_res-4),(logo_pos*5, y_res-25)), fill=(0,255,0), width=3)
+
+    return image
+
+# This draws all the elements of the hud that change, it is called repeatedly.
+def drawHUD(HUD):
+    image = HUD.copy()
     draw = ImageDraw.Draw(image)
-    draw.text((20, 70), "Testing 123", fill=(255,255,255))
-    draw.text((20, 100), "CPU Temp : %s" % measure_temp())
-    draw.text((20, 130), "WiFi : %s" % getWifiStats())
+
+    text_spacing = x_res / 6
+    cpu_usage = psutil.cpu_percent(percpu=True)
+
+    draw.text((10, y_res-20), "CPU Temp : %s" % measure_temp())
+    draw.text((text_spacing+5, y_res-20), "CPU Usage : %s%% %s%% %s%% %s%%" % tuple(cpu_usage))
+    draw.text((5*text_spacing+5, y_res-20), "WiFi : %s" % getWifiStats())
     return(image) 
 
 def start():
