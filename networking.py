5c87795b06dcc9cdc0ccf66749a982d47171425d
Bryan Morrison <skeeter_mcbee@live.com>
2019-01-25 14:26:23 -0800

    Update networking.py

--- networking.py
+++ networking.py
@@ -46,7 +46,7 @@ secure_cert = None
 
 onHandleChatMesasge = None
 
-bootMessage = None
+#bootMessage = None
 
 def getControlHostPort():
     url = 'https://%s/get_control_host_port/%s?version=2' % (infoServer, robot_id)
@@ -250,7 +250,7 @@ def setupSocketIO(robot_config):
     global messengerName
     global messengerUsername
     global messengerPassword
-    global bootMessage
+    #global bootMessage
 
     robot_id = robot_config.get('robot', 'robot_id')
     camera_id = robot_config.getint('robot', 'camera_id')
@@ -266,7 +266,7 @@ def setupSocketIO(robot_config):
     messengerPassword = robot_config.get('messenger', 'password')
     messengerName = robot_config.get('messenger', 'robot_name')
 
-    bootMessage = robot_config.get('tts', 'boot_message')
+    #bootMessage = robot_config.get('tts', 'boot_message')
 
     controlHostPort = getControlHostPort()
     chatHostPort = getChatHostPort()
