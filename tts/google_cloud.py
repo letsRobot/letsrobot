9c873bae885fe50c63bbc676911ec073c4f68f21
Bryan Morrison <skeeter_mcbee@live.com>
2019-01-30 19:30:11 -0800

    add google cloud tts

---
+++ tts/google_cloud.py
@@ -0,0 +1,55 @@
+import os
+import tempfile
+import uuid
+import logging
+
+from google.cloud import texttospeech
+
+log = logging.getLogger('LR.tts.google_cloud')
+
+client = None
+voice = None
+hwNum = None
+audio_config = None
+
+client = None
+
+
+def setup(robot_config):
+    global client
+    global voice
+    global hwNum
+    global audio_config
+
+    client = texttospeech.TextToSpeechClient()
+
+    if os.getenv("GOOGLE_APPLICATION_CREDENTIALS") == None:
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/pi/key.json"
+        log.info("setting tts env variable")
+
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3
+    )
+
+    hwNum = robot_config.get('tts', 'hw_num')
+
+
+def say(*args):
+    global client
+    global voice
+    global hwNum
+    global audio_config
+
+    message = args[0]
+
+    synthesis_input = texttospeech.types.SynthesisInput(text=message)
+
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+
+    with open('output.mp3', 'wb') as out:
+        out.write(response.audio_content)
+        os.system('/usr/bin/mpg123-alsa -a hw:' + hwNum + ',0 output.mp3')
