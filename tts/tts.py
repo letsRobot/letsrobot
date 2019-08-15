5ee2e2c89b2c457c5ac88d92b2950a3e7c822680
bmorrison4 <skeeter_mcbee@live.com>
2019-08-14 17:04:20 -0700

    try new amixer command

--- tts/tts.py
+++ tts/tts.py
@@ -170,8 +170,11 @@ def volume(vol):
         tts_module.volume(vol)
     except AttributeError:
         log.info("Setting volume to %d" % new_vol)
-        os.system("amixer set PCM -- 100%d%%" % new_vol)
-        os.system("amixer -c %s cset numid=3 %d%%" % (hw_num, new_vol))
+        # os.system("amixer set PCM -- 100%d%%" % new_vol)
+        # os.system("amixer -c %s cset numid=3 %d%%" % (hw_num, new_vol))
+        os.system("amixer -c %s set PCM %s%%" % (hw_num, new_vol))
+        
+
     
 def onHandleChatMessageRemoved(*args):
    global tts_deleted
