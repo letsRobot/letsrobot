bb1191722e3b0f4f779090430f660c06295979a7
Ncoturnal42 <steele.peter@gmail.com>
2020-01-22 03:05:47 +1100

    Added windows support for espeak.
    - Updated espeak to check for windows and call differently depending on if it is running on windows or not.
    - Added an optional espeak_path setting in the [espeak] conf setion.

--- tts/espeak.py
+++ tts/espeak.py
@@ -9,12 +9,14 @@ tempDir = None
 male = None
 voice_number = None
 hw_num = None
+espeak_path = None
 
 def setup(robot_config):
     global tempDir
     global male
     global voice_number
     global hw_num
+    global espeak_path
 
     male = robot_config.getboolean('espeak', 'male')
     voice_number = robot_config.getint('espeak', 'voice_number')
@@ -24,6 +26,13 @@ def setup(robot_config):
     else:
         hw_num = robot_config.get('tts', 'hw_num')
 
+    if robot_config.has_option('espeak', 'espeak_path'):
+        espeak_path = robot_config.get('espeak', 'espeak_path')
+    else:
+        if os.name == 'nt':
+            espeak_path = '"c:\\Program Files (x86)\\eSpeak\\command_line\\espeak.exe"'
+        else:
+            espeak_path = '/usr/bin/espeak'
 
     #set the location to write the temp file to
     tempDir = tempfile.gettempdir()
@@ -38,7 +47,13 @@ def say(*args):
     f.close()
 
     if male:
-        os.system('cat ' + tempFilePath + ' | espeak -v en-us+m{} -s 170 --stdout | aplay -D plughw:{}'.format(voice_number, hw_num) )
+        espeak_command = espeak_path + ' -v en-us+m{} -s 170'.format(voice_number)
     else:
-        os.system('cat ' + tempFilePath + ' | espeak -v en-us+f{} -s 170 --stdout | aplay -D plughw:{}'.format(voice_number, hw_num) )
+        espeak_command = espeak_path + ' -v en-us+f{} -s 170'.format(voice_number)
+
+    if os.name == 'nt':
+        os.system('type ' + tempFilePath + ' | ' + espeak_command )
+    else:
+        os.system('cat ' + tempFilePath + ' | ' + espeak_command + ' --stdout | aplay -D plughw:{}'.format(hw_num) )
+
     os.remove(tempFilePath)    
