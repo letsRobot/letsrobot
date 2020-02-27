96c470788c6d4c06d6fddaf2e0878fa79ecbf32d
Nocturnal <steele.peter@gmail.com>
2020-02-27 16:22:57 +1100

    Updated adafruit_pwm - Updated hardware/adafruit_pwm/py to use the newer Adafruit_PCA9685 since the original module is no longer supported or easily available.

--- hardware/adafruit_pwm.py
+++ hardware/adafruit_pwm.py
@@ -1,10 +1,15 @@
-from Adafruit_PWM_Servo_Driver import PWM
+import Adafruit_PCA9685
 import logging
+import time
+
 log = logging.getLogger('RemoTV.hardware.adafruit_pwm')
 
+pwm = None
+
 def setup(robot_config):
-    pwm = PWM(int(robot_config.get('adafruit_pwm', 'address'),16)) 
-    pwm.setPWMFreq(robot_config.getint('adafruit_pwm', 'pwm_freq'))      # Set frequency to 60 Hz
+    global pwm
+    pwm = Adafruit_PCA9685.PCA9685(int(robot_config.get('adafruit_pwm', 'address'),16)) 
+    pwm.set_pwm_freq(robot_config.getint('adafruit_pwm', 'pwm_freq'))      # Set frequency to 60 Hz
     
 def move(args):
     command = args['button']['command']
@@ -12,57 +17,57 @@ def move(args):
     log.debug("move adafruit pwm command : %s", command)
         
     if command == 'l':
-        pwm.setPWM(1, 0, 300) # turn left
-        pwm.setPWM(0, 0, 445) # drive forward
+        pwm.set_pwm(1, 0, 300) # turn left
+        pwm.set_pwm(0, 0, 445) # drive forward
         time.sleep(0.5)
-        pwm.setPWM(1, 0, 400) # turn neutral
-        pwm.setPWM(0, 0, 335) # drive neutral
+        pwm.set_pwm(1, 0, 400) # turn neutral
+        pwm.set_pwm(0, 0, 335) # drive neutral
 
     if command == 'r':
-        pwm.setPWM(1, 0, 500) # turn right
-        pwm.setPWM(0, 0, 445) # drive forward
+        pwm.set_pwm(1, 0, 500) # turn right
+        pwm.set_pwm(0, 0, 445) # drive forward
         time.sleep(0.5)
-        pwm.setPWM(1, 0, 400) # turn neutral
-        pwm.setPWM(0, 0, 335) # drive neutral
+        pwm.set_pwm(1, 0, 400) # turn neutral
+        pwm.set_pwm(0, 0, 335) # drive neutral
 
     if command == 'bl':
-        pwm.setPWM(1, 0, 300) # turn left
-        pwm.setPWM(0, 0, 270) # drive backward
+        pwm.set_pwm(1, 0, 300) # turn left
+        pwm.set_pwm(0, 0, 270) # drive backward
         time.sleep(0.5)
-        pwm.setPWM(1, 0, 400) # turn neutral
-        pwm.setPWM(0, 0, 335) # drive neutral
+        pwm.set_pwm(1, 0, 400) # turn neutral
+        pwm.set_pwm(0, 0, 335) # drive neutral
 
     if command == 'br':
-        pwm.setPWM(1, 0, 500) # turn right
-        pwm.setPWM(0, 0, 270) # drive backward
+        pwm.set_pwm(1, 0, 500) # turn right
+        pwm.set_pwm(0, 0, 270) # drive backward
         time.sleep(0.5)
-        pwm.setPWM(1, 0, 400) # turn neurtral
-        pwm.setPWM(0, 0, 335) # drive neutral
+        pwm.set_pwm(1, 0, 400) # turn neurtral
+        pwm.set_pwm(0, 0, 335) # drive neutral
 
         
     if command == 'f':
-        pwm.setPWM(0, 0, 445) # drive forward
+        pwm.set_pwm(0, 0, 445) # drive forward
         time.sleep(0.3)
-        pwm.setPWM(0, 0, 345) # drive slowly forward
+        pwm.set_pwm(0, 0, 345) # drive slowly forward
         time.sleep(0.4)
-        pwm.setPWM(0, 0, 335) # drive neutral
+        pwm.set_pwm(0, 0, 335) # drive neutral
     if command == 'b':
-        pwm.setPWM(0, 0, 270) # drive backward
+        pwm.set_pwm(0, 0, 270) # drive backward
         time.sleep(0.3)
-        pwm.setPWM(0, 0, 325) # drive slowly backward
+        pwm.set_pwm(0, 0, 325) # drive slowly backward
         time.sleep(0.4)
-        pwm.setPWM(0, 0, 335) # drive neutral
+        pwm.set_pwm(0, 0, 335) # drive neutral
 
     if command == 'S2INC': # neutral
-        pwm.setPWM(2, 0, 300)
+        pwm.set_pwm(2, 0, 300)
 
     if command == 'S2DEC':
-        pwm.setPWM(2, 0, 400)
+        pwm.set_pwm(2, 0, 400)
 
     if command == 'POS60':
-        pwm.setPWM(2, 0, 490)        
+        pwm.set_pwm(2, 0, 490)        
 
     if command == 'NEG60':
-        pwm.setPWM(2, 0, 100)                
+        pwm.set_pwm(2, 0, 100)                
     
 
